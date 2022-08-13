6bcb76ff6eb53a092a0b706bae39f8b8df84a23b
jungmin <kimjm0825@gmail.com>
2022-08-14 00:09:02 +0900

    fragment 터치 애니메이션 및 recycler view 코드 수정

--- YouTube_part4_chatper01/app/src/main/java/com/example/youtube_part4_chapter01/PlayerFragment.kt
+++ YouTube_part4_chatper01/app/src/main/java/com/example/youtube_part4_chapter01/PlayerFragment.kt
@@ -1,15 +1,27 @@
 package com.example.youtube_part4_chapter01
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.youtube_part4_chapter01.adapter.VideoAdapter
 import com.example.youtube_part4_chapter01.databinding.FragmentPlayerBinding
+import com.example.youtube_part4_chapter01.dto.VideoDto
+import com.example.youtube_part4_chapter01.service.VideoService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.abs
 
 class PlayerFragment: Fragment(R.layout.fragment_player) {
 
     private var binding: FragmentPlayerBinding? = null
+    private lateinit var videoAdapter: VideoAdapter
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,6 +29,13 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
         binding = fragmentPlayerBinding
 
+        initMotionLayoutEvent(fragmentPlayerBinding)
+        initRecyclerView(fragmentPlayerBinding)
+
+        getVideoList()
+    }
+
+    private fun initMotionLayoutEvent(fragmentPlayerBinding: FragmentPlayerBinding) {
         fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object: MotionLayout.TransitionListener {
             override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
 
@@ -45,6 +64,54 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
             }
 
         })
+    }
+
+    private fun initRecyclerView(fragmentPlayerBinding: FragmentPlayerBinding) {
+
+        videoAdapter = VideoAdapter(callback = { url, title ->
+            play(url, title)
+        })
+
+        fragmentPlayerBinding.fragmentRecyclerView.apply {
+            adapter = videoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun getVideoList() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(VideoService::class.java).also {
+            it.listVideos()
+                .enqueue(object : Callback<VideoDto> {
+                    override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
+                        if (response.isSuccessful.not()) {
+                            Log.d("MainActivity", "response fail")
+                            return
+                        }
+
+                        response.body()?.let { videoDto ->
+                            Log.d("MainActivity", it.toString())
+                            videoAdapter.submitList(videoDto.videos)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<VideoDto>, t: Throwable) {
+                        // 예외처리
+                    }
+
+                })
+        }
+    }
+
+    fun play(url: String, title: String) {
+        binding?.let {
+            it.playerMotionLayout.transitionToEnd()
+            it.bottomTitleTextView.text = title
+        }
     }
 
     override fun onDestroy() {
