f49f23da4c23777a2a8dd1c2a3dee2b82ba0b57f
jungmin <kimjm0825@gmail.com>
2022-08-16 17:42:26 +0900

    멜론 앱 프로젝트 생성

---
+++ Melon_part4_chatper02/app/src/main/java/com/example/melon_part4_chapter02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.melon_part4_chapter02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
