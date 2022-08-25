aa1651b0e5f885b9ecb44377aecbd9fdef57a9f8
jungmin <kimjm0825@gmail.com>
2022-08-25 18:37:47 +0900

    retrofit service 작성

---
+++ Google_Map_part4_chatper03/app/src/main/java/com/example/google_map_part4_chatper03/Key.kt
@@ -0,0 +1,5 @@
+package com.example.google_map_part4_chatper03
+
+object Key {
+    const val TMAP_API = ""
+}
--- Google_Map_part4_chatper03/app/src/main/java/com/example/google_map_part4_chatper03/MainActivity.kt
+++ Google_Map_part4_chatper03/app/src/main/java/com/example/google_map_part4_chatper03/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.google_map_part4_chatper03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.google_map_part4_chatper03.databinding.ActivityMainBinding
 import com.example.google_map_part4_chatper03.model.LocationLatLngEntity
 import com.example.google_map_part4_chatper03.model.SearchResultEntity
+import com.example.google_map_part4_chatper03.response.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: SearchRecyclerAdapter
@@ -20,8 +30,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        job = Job()
+
         initAdapter()
         initViews()
+        bindViews()
         initData()
         setData()
     }
@@ -29,6 +42,12 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() = with(binding) {
         emptyResultTextView.isVisible = false
         recyclerView.adapter = adapter
+    }
+
+    private fun bindViews() = with(binding) {
+        searchButton.setOnClickListener {
+            searchKeyword(searchBarInputView.text.toString())
+        }
     }
 
     private fun initAdapter() {
@@ -54,5 +73,25 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "빌딩이름 : ${it.name}, 주소 : ${it.fullAddress}", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun searchKeyword(keywordString: String) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = RetrofitUtil.apiService.getSearchLocation(
+                        keyword = keywordString
+                    )
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            Log.e("response", body.toString())
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+
+            }
+        }
     }
 }
