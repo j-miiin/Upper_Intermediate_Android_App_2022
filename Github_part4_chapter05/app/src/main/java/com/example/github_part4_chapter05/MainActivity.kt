2d97ec51005ce7e4d1aa3e703205cc93a0c5991b
jungmin <kimjm0825@gmail.com>
2022-11-22 14:22:04 +0900

    깃허브 연결

--- Github_part4_chapter05/app/src/main/java/com/example/github_part4_chapter05/MainActivity.kt
+++ Github_part4_chapter05/app/src/main/java/com/example/github_part4_chapter05/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.github_part4_chapter05
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import com.example.github_part4_chapter05.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    // todo https://github.com/login/oauth/authorize?client_id=abcdef
+    private fun loginGithub() {
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this, loginUri)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let {
+            // todo getAccessToken
+        }
     }
 }
