918bfcd932f6343b230241a97dc3bb7d6e06e6c8
jungmin <kimjm0825@gmail.com>
2022-11-28 22:04:50 +0900

    ui 작성 및 사진 검색 기능 구현

--- Free_Image_part4_chapter07/app/src/main/java/com/example/free_image_part4_chapter07/MainActivity.kt
+++ Free_Image_part4_chapter07/app/src/main/java/com/example/free_image_part4_chapter07/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.free_image_part4_chapter07
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.free_image_part4_chapter07.data.Repository
+import com.example.free_image_part4_chapter07.databinding.ActivityMainBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = MainScope()
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initViews()
+        bindViews()
+        fetchRandomPhotos()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
+
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.adapter = PhotoAdapter()
+    }
+
+    private fun bindViews() {
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
+                    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+
+                    view.clearFocus()
+                }
+
+                fetchRandomPhotos(editText.text.toString())
+            }
+
+            true
+        }
+
+        binding.refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.text.toString())
+        }
+    }
+
+    private fun fetchRandomPhotos(query: String? = null) = scope.launch{
+        Repository.getRandomPhotos(query)?.let { photos ->
+            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                this.photos = photos
+                notifyDataSetChanged()
+            }
+
+            binding.refreshLayout.isRefreshing = false
+        }
     }
 }
