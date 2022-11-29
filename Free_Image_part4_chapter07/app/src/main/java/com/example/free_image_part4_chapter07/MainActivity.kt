72e322507e041cd53e803ffbce87bcc29ddd67ed
jungmin <kimjm0825@gmail.com>
2022-11-29 15:44:35 +0900

    사진 저장 기능 구현

--- Free_Image_part4_chapter07/app/src/main/java/com/example/free_image_part4_chapter07/MainActivity.kt
+++ Free_Image_part4_chapter07/app/src/main/java/com/example/free_image_part4_chapter07/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.free_image_part4_chapter07
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.free_image_part4_chapter07.data.Repository
+import com.example.free_image_part4_chapter07.data.models.PhotoResponse
 import com.example.free_image_part4_chapter07.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
@@ -27,12 +43,33 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         bindViews()
-        fetchRandomPhotos()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            fetchRandomPhotos()
+        } else {
+            requestWriteStoragePermission()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val writeExternalStoragePermissionGranted =
+            requestCode == REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (writeExternalStoragePermissionGranted) {
+            fetchRandomPhotos()
+        }
     }
 
     private fun initViews() {
@@ -59,6 +96,18 @@ class MainActivity : AppCompatActivity() {
         binding.refreshLayout.setOnRefreshListener {
             fetchRandomPhotos(binding.searchEditText.text.toString())
         }
+
+        (binding.recyclerView.adapter as? PhotoAdapter)?.onClickPhoto = { photo ->
+            showDownloadPhotoConfirmationDialog(photo)
+        }
+    }
+
+    private fun requestWriteStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION
+        )
     }
 
     private fun fetchRandomPhotos(query: String? = null) = scope.launch{
@@ -79,4 +128,92 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private fun showDownloadPhotoConfirmationDialog(photo: PhotoResponse) {
+        AlertDialog.Builder(this)
+            .setMessage("이 사진을 저장하시겠습니까?")
+            .setPositiveButton("저장") { dialog, _ ->
+                downloadPhoto(photo.urls?.full)
+                dialog.dismiss()
+            }
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun downloadPhoto(photoUrl: String?) {
+        photoUrl ?: return
+
+        Glide.with(this)
+            .asBitmap()
+            .load(photoUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(
+                object : CustomTarget<Bitmap>(SIZE_ORIGINAL, SIZE_ORIGINAL) {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        saveBitmapToMediaStore(resource)
+                    }
+
+                    override fun onLoadStarted(placeholder: Drawable?) {
+                        super.onLoadStarted(placeholder)
+                        Snackbar.make(binding.root, "다운로드 중...", Snackbar.LENGTH_INDEFINITE).show()
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) = Unit
+
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        super.onLoadFailed(errorDrawable)
+                        Snackbar.make(binding.root, "다운로드 실패", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+
+            )
+    }
+
+    private fun saveBitmapToMediaStore(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        val resolver = applicationContext.contentResolver
+        val imageCollectionUri =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+            }
+        }
+
+        val imageUri = resolver.insert(imageCollectionUri, imageDetails)
+
+        imageUri ?: return
+
+        // kotlin에서 제공하는 use 블럭 사용 시, Stream을 닫아주는 동작까지 해줌
+        resolver.openOutputStream(imageUri).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            imageDetails.clear()
+            imageDetails.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(imageUri, imageDetails, null, null)
+        }
+
+        Snackbar.make(binding.root, "다운로드 완료", Snackbar.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 101
+    }
 }
